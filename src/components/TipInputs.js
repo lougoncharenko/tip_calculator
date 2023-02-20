9d3bfc8f88dd19a4ce564a82266f42827e53ac29
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-02-20 14:25:49 -0500

    creating inputs

--- src/components/TipInputs.js
+++ src/components/TipInputs.js
@@ -3,7 +3,21 @@ import React,{useState} from "react";
   export default function TipInputs () {
     return (
         <section>
-            <h1>Tip Calculator</h1>
+            <form>
+                <label>
+                    Bill Amount:
+                    <input type="text" name="bill" />
+                </label>
+                <label>
+                    Tip Percent:
+                    <input type="text" name="percent" />
+                </label>
+                <label>
+                    Number of people to split between:
+                    <input type="text" name="percent" />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
         </section>
     );
   }
