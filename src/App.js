6cee313718eb3b4dfb3e1870c1e8036ba25d91b8
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-02-20 14:51:11 -0500

    imported headed and inputs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import TipInputs from './components/TipInputs';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <TipInputs />
     </div>
   );
 }
