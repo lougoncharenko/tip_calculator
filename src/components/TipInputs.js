da77bf59f23c48df29a05328c54b75923b2c4a80
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-02-22 11:06:52 -0500

    added calculate method

--- src/components/TipInputs.js
+++ src/components/TipInputs.js
@@ -1,7 +1,20 @@
-import React,{useState} from "react";
+import React from "react";
 import '../styles/TipInputs.css'
   
-  export default function TipInputs () {
+  export default function TipInputs ({bill, setBill, percent, setPercent, people, setPeople, setAmount}) {
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        calculate(bill, percent, people)
+    }
+
+    const calculate = (bill, percent, people) => {
+        const decimal = Number(percent)/100
+        const billPlusTip = Number(bill) * (1+ decimal)
+        const perPerson = billPlusTip/Number(people)
+        setAmount(perPerson)
+        console.log(perPerson)
+    }
+
     return (
         <section className="form-input">
             <form>
@@ -10,22 +23,36 @@ import '../styles/TipInputs.css'
                     <input 
                     type="text" 
                     name="bill" 
-                    placeholder="$0.00"/>
+                    placeholder="$0.00"
+                    value = {bill}
+                    onChange={e => setBill(e.target.value)}
+                    />
                 </label>
                 <label>
                     Tip Percent:
                     <input 
                     type="text"
-                     name="percent" 
-                     placeholder="%"/>
+                    name="percent" 
+                    placeholder="%"
+                    value = {percent}
+                    onChange={e => setPercent(e.target.value)}
+                     />
                 </label>
                 <label>
                     Split between:
-                    <input type="text" 
+                    <input 
+                    type="text" 
                     name="percent"
-                    placeholder="Number of people" />
+                    placeholder="Number of people" 
+                    value = {people}
+                    onChange={e => setPeople(e.target.value)}
+                    />
                 </label>
-                <input type="submit" value="Submit" />
+                <input 
+                type="submit" 
+                value="Submit" 
+                onClick={handleSubmit}
+                />
             </form>
         </section>
     );
