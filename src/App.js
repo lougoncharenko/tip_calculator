5298f11c4cbfff3c59153048f00c8ef3d9db3b34
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-02-22 11:06:31 -0500

    added usestate and imported display

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
+import React,{useState} from "react";
 import './App.css';
 import Header from './components/Header';
 import TipInputs from './components/TipInputs';
+import Display from './components/Display';
 
 function App() {
+  const [bill, setBill] = useState('');
+  const [percent, setPercent] = useState('');
+  const [people, setPeople] = useState('')
+const [amount, setAmount] = useState({})
+
   return (
     <div className="App">
       <Header />
-      <TipInputs />
+      <TipInputs 
+      bill = {bill}
+      setBill = {setBill}
+      percent = {percent}
+      setPercent = {setPercent}
+      people = {people}
+      setPeople ={setPeople}
+      setAmount = {setAmount}
+      />
+     
+      <Display 
+      bill={bill}
+      percent={percent}
+      people={people}
+      amount = {amount}
+      />
     </div>
   );
 }
